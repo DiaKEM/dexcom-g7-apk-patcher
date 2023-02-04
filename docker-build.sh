485bbf417624a6096da5de7af7b8dadf8b1be8d1
Arakon <Arakon@users.noreply.github.com>
2023-02-04 20:48:27 +0100

    Fix for not updating the docker image
    Enforces full download of the docker image to avoid issues with changes to the scripts.

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf dexcom.patched.apk
-docker build -t diakem-dexcom-g7-apk-patcher .
+docker build --no-cache -t diakem-dexcom-g7-apk-patcher .
 docker run -it -v $(pwd):/output diakem-dexcom-g7-apk-patcher 
